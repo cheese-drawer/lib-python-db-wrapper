c39c6c0ebb6eacdf4b4d5869e181c187569f6278
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-03-14 16:43:42 -0400

    Convert ModelData from TypedDict to BaseModel
    - Uses pydantic's BaseModel instead
    - Adds runtime type validation at the Model level
    - Breaking change, requires building Models with the class Constructor
    instead of simply passing dictionaries around

--- tests/test_model.py
+++ tests/test_model.py
@@ -13,9 +13,8 @@ from typing import (
     TypeVar,
     List,
     Tuple,
-    TypedDict,
 )
-from uuid import uuid4, UUID
+from uuid import uuid4
 import unittest
 from unittest import TestCase
 
@@ -64,34 +63,34 @@ class TestReadOneById(TestCase):
 
     @helpers.async_test
     async def test_it_correctly_builds_query_with_given_id(self) -> None:
-        item: ModelData = {'_id': uuid4()}
+        item = ModelData(id=uuid4())
         model, client = setup([item])
-        await model.read.one_by_id(str(item['_id']))
+        await model.read.one_by_id(str(item.id))
         query_composed = cast(
             helpers.AsyncMock, client.execute_and_return).call_args[0][0]
         query = helpers.composed_to_string(query_composed)
 
         self.assertEqual(query, "SELECT * "
                                 "FROM test "
-                                f"WHERE _id = {item['_id']};")
+                                f"WHERE id = {item.id};")
 
     @helpers.async_test
     async def test_it_returns_a_single_result(self) -> None:
-        item: ModelData = {'_id': uuid4()}
+        item = ModelData(id=uuid4())
         model, _ = setup([item])
-        result = await model.read.one_by_id(str(item['_id']))
+        result = await model.read.one_by_id(str(item.id))
 
         self.assertEqual(result, item)
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_raises_exception_if_more_than_one_result(self) -> None:
-        item: ModelData = {'_id': uuid4()}
+        item = ModelData(id=uuid4())
         model, _ = setup([item, item])
 
         with self.assertRaises(UnexpectedMultipleResults):
-            await model.read.one_by_id(str(item['_id']))
+            await model.read.one_by_id(str(item.id))
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_raises_exception_if_no_result_to_return(self) -> None:
         model: Model[ModelData]
         model, _ = setup([])
@@ -108,13 +107,13 @@ class TestCreateOne(TestCase):
         a: str
         b: str
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_correctly_builds_query_with_given_data(self) -> None:
-        item: TestCreateOne.Item = {
-            '_id': uuid4(),
+        item = TestCreateOne.Item(**{
+            'id': uuid4(),
             'a': 'a',
             'b': 'b',
-        }
+        })
         model, client = setup([item])
 
         await model.create.one(item)
@@ -122,17 +121,17 @@ class TestCreateOne(TestCase):
             helpers.AsyncMock, client.execute_and_return).call_args[0][0]
         query = helpers.composed_to_string(query_composed)
 
-        self.assertEqual(query, 'INSERT INTO test (_id,a,b) '
-                                f"VALUES ({item['_id']},a,b) "
+        self.assertEqual(query, 'INSERT INTO test (id,a,b) '
+                                f"VALUES ({item.id},a,b) "
                                 'RETURNING *;')
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_returns_the_new_record(self) -> None:
-        item: TestCreateOne.Item = {
-            '_id': uuid4(),
+        item = TestCreateOne.Item(**{
+            'id': uuid4(),
             'a': 'a',
             'b': 'b',
-        }
+        })
         model, _ = setup([item])
 
         result = await model.create.one(item)
@@ -148,84 +147,84 @@ class TestUpdateOne(TestCase):
         a: str
         b: str
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_correctly_builds_query_with_given_data(self) -> None:
-        item: TestUpdateOne.Item = {
-            '_id': uuid4(),
+        item = TestUpdateOne.Item(**{
+            'id': uuid4(),
             'a': 'a',
             'b': 'b',
-        }
+        })
         # cast required to avoid mypy error due to unpacking
         # TypedDict, see more on GitHub issue
         # https://github.com/python/mypy/issues/4122
-        updated = cast(TestUpdateOne.Item, {**item, 'b': 'c'})
+        updated = TestUpdateOne.Item(**{**item.dict(), 'b': 'c'})
         model, client = setup([updated])
 
-        await model.update.one_by_id(str(item['_id']), {'b': 'c'})
+        await model.update.one_by_id(str(item.id), {'b': 'c'})
         query_composed = cast(
             helpers.AsyncMock, client.execute_and_return).call_args[0][0]
         query = helpers.composed_to_string(query_composed)
 
         self.assertEqual(query, 'UPDATE test '
                                 'SET b = c '
-                                f"WHERE _id = {item['_id']} "
+                                f"WHERE id = {item.id} "
                                 'RETURNING *;')
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_returns_the_new_record(self) -> None:
-        item: TestUpdateOne.Item = {
-            '_id': uuid4(),
+        item = TestUpdateOne.Item(**{
+            'id': uuid4(),
             'a': 'a',
             'b': 'b',
-        }
+        })
         # cast required to avoid mypy error due to unpacking
         # TypedDict, see more on GitHub issue
         # https://github.com/python/mypy/issues/4122
-        updated = cast(TestUpdateOne.Item, {**item, 'b': 'c'})
+        updated = TestUpdateOne.Item(**{**item.dict(), 'b': 'c'})
         model, _ = setup([updated])
 
-        result = await model.update.one_by_id(str(item['_id']), {'b': 'c'})
+        result = await model.update.one_by_id(str(item.id), {'b': 'c'})
 
         self.assertEqual(result, updated)
 
 
 class TestDeleteOneById(TestCase):
-    """Testing Model.delete.one_by_id"""
+    """Testing Model.delete.one_byid"""
 
     class Item(ModelData):
         """Example ModelData Item for testing."""
         a: str
         b: str
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_correctly_builds_query_with_given_data(self) -> None:
-        item: TestDeleteOneById.Item = {
-            '_id': uuid4(),
+        item = TestDeleteOneById.Item(**{
+            'id': uuid4(),
             'a': 'a',
             'b': 'b',
-        }
+        })
         model, client = setup([item])
 
-        await model.delete.one_by_id(str(item['_id']))
+        await model.delete.one_by_id(str(item.id))
 
         query_composed = cast(
             helpers.AsyncMock, client.execute_and_return).call_args[0][0]
         query = helpers.composed_to_string(query_composed)
 
         self.assertEqual(query, 'DELETE FROM test '
-                                f"WHERE id = {item['_id']} "
+                                f"WHERE id = {item.id} "
                                 'RETURNING *;')
 
-    @helpers.async_test
+    @ helpers.async_test
     async def test_it_returns_the_deleted_record(self) -> None:
-        item: TestDeleteOneById.Item = {
-            '_id': uuid4(),
+        item = TestDeleteOneById.Item(**{
+            'id': uuid4(),
             'a': 'a',
             'b': 'b',
-        }
+        })
         model, _ = setup([item])
 
-        result = await model.delete.one_by_id(str(item['_id']))
+        result = await model.delete.one_by_id(str(item.id))
 
         self.assertEqual(result, item)
 

f7f7c78e8e5706443450b7e3998347b3e6b08e8c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2021-10-25 09:08:01 -0400

    Bump to 2.1.1.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ short_description = \
 
 setuptools.setup(
     name="db_wrapper",
-    version="2.1.0",
+    version="2.1.1",
     author="Andrew Chang-DeWitt",
     author_email="andrew@andrew-chang-dewitt.dev",
     description=short_description,
